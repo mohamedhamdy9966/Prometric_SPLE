a28f56644b8525d5f5398b753470236013122c7c
mohamedhamdy9966 <mh7008002@gmail.com>
2024-07-14 14:36:04 +0300

    add

--- js/js.js
+++ js/js.js
@@ -102,7 +102,7 @@ function login(usernameInput, passwordInput) {
         "rawan": "othman@2024",
         "rahaf" : "rahaf@2024",
         "omar" : "alanzi@2024",
-        "user990": "user990@2024",
+        "user": "user990@2024",
         "hawary": "hawary@2024",
         "ranakarim" : "rana@2024",
         "nada" : "kassem@2024",
